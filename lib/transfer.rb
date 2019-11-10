93d3e96dfd28a9c4a93b8d16a4a4718b540c4d92
Ruel Tingling <rvtingling@gmail.com>
2019-11-10 07:11:03 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,7 +17,7 @@ class Transfer
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = "complete"
-      @sender.balance < @amount && sender.valid 
+      @sender.balance < @amount 
     else
       @status = "rejected"
       return "Transaction rejected. Please check your account balance."
